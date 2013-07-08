8cfd3b7bbd0603c9c013594cb825afa3fe45586b
RRails <sanjita_quinn@hotmail.com>
2013-07-09 08:44:13 +1000

    Updates for converting time to UTC

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -9,7 +9,12 @@ class CallsController < ApplicationController
 
   def create
     call = Call.create(params[:call])
+    # how do I get the call owner's timezone?
+    Time.zone = params[:call.owner][:timezone]
+    call.time = Time.use_zone(Time.zone) {Time.zone.parse(params[:call][:time]).in_time_zone(Time.zone)}
+    call.save
     call.attendees.each do |attendee|
+      # need to store the meeting date and time in UTC
       attendee.meetingdate = call.date
       attendee.accepted = FALSE # add this to database level
       attendee.save
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    binding.pry
+
   end
 
   def new
@@ -9,19 +11,45 @@ class UsersController < ApplicationController
 
   def create
     user = User.create(params[:user])
+    # Convert times from user timezone to UTC
+    Time.zone = params[:user][:timezone]
+    user.preferredstarttime = Time.use_zone(Time.zone) {Time.zone.parse(params[:user][:preferredstarttime]).in_time_zone(Time.zone)}
+    user.preferredendtime = Time.use_zone(Time.zone) {Time.zone.parse(params[:user][:preferredendtime]).in_time_zone(Time.zone)}
+    user.save
+
     redirect_to(user)
   end
 
   def show
     @user = User.find(params[:id])
+# not sure if this code is needed.
+
+    # Time.zone = @user.timezone
+    # binding.pry
+    # @user.preferredstarttime = Time.use_zone(Time.zone) {Time.zone.parse(params[:user][:preferredstarttime]).in_time_zone(Time.zone)}
+    # @user.preferredendtime = Time.use_zone(Time.zone) {Time.zone.parse(params[:user][:preferredendtime]).in_time_zone(Time.zone)}
+
   end
 
   def edit
     @user = User.find(params[:id]) #find the user we want to update
+    Time.zone = @user.timezone
+    binding.pry
+# not sure if this code is needed.
+
+    # @user.preferredstarttime = Time.use_zone(Time.zone){Time.zone.parse(@user.preferredstarttime).in_time_zone(Time.zone)}
+    # @user.preferredendtime = Time.use_zone(Time.zone){Time.zone.parse(@user.preferredendtime).in_time_zone(Time.zone)}
+
   end
 
   def update
     user = User.find(params[:id]) #find the user we want to update
+    # convert the time from user timezone to UTC
+    Time.zone = params[:user][:timezone]
+    user.preferredstarttime = Time.use_zone(Time.zone) {Time.zone.parse(params[:user][:preferredstarttime]).in_time_zone(Time.zone)}
+    user.preferredendtime = Time.use_zone(Time.zone) {Time.zone.parse(params[:user][:preferredendtime]).in_time_zone(Time.zone)}
+    user.save
+
     user.update_attributes(params[:user])
     redirect_to(users_path)
   end
